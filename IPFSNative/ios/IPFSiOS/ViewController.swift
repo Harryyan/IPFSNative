9b3ad79b1d22a9162e45602a7c0182d28111b06c
harryyan1238 <harryyan1238@gmail.com>
2018-09-15 13:47:33 +1200

    add iOS project and install 3rd party lib

---
+++ IPFSNative/ios/IPFSiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IPFSiOS
+//
+//  Created by DearLan on 15/09/18.
+//  Copyright © 2018 com.nz.IPFS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
