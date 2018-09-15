3f7fa5d2840a53b88353375d40eef0941ac59e63
harryyan1238 <harryyan1238@gmail.com>
2018-09-15 13:41:36 +1200

    add android project

---
+++ IPFSNative/android/app/src/main/java/com/example/harry/ipfsandroid/ReactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.harry.ipfsandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_react)
+    }
+}
--- IPFSNative/ios/Pods/Target Support Files/Pods-IPFSNative-tvOSTests/Pods-IPFSNative-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IPFSNative_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_IPFSNative_tvOSTests
-@end
